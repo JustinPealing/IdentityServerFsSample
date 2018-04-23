78ce922cfa0c80a5b53f0863dc15ca58cbd8e9fb
Justin Pealing <justin@pealing.me.uk>
2018-04-23 20:07:38 +0100

    Initial version

---
+++ src/AuthServer/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Builder
+
+let apiResources = []
+
+let clients = []
+
+let configureServices (services : IServiceCollection) =
+    services.AddIdentityServer()
+        .AddDeveloperSigningCredential()
+        .AddInMemoryApiResources(apiResources)
+        .AddInMemoryClients(clients) |> ignore
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseDeveloperExceptionPage() |> ignore
+    app.UseIdentityServer() |> ignore
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    WebHostBuilder()
+        .UseKestrel()
+        .ConfigureServices(configureServices)
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .Build()
+        .Run()
+    0
