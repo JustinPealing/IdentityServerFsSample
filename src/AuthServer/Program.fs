a5fd43e8ce32e13afe9771b189c0683dbd3883ef
Justin Pealing <justin@pealing.me.uk>
2018-04-25 20:41:09 +0100

    Hook up Razor

--- src/AuthServer/Program.fs
+++ src/AuthServer/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.IO
 open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
@@ -6,11 +7,12 @@ open Microsoft.Extensions.Logging
 open Microsoft.AspNetCore.Builder
 open IdentityServer4.Models
 open Giraffe
+open Giraffe.Razor
 
 let webApp =
     choose [
         route "/ping"   >=> text "pong"
-        route "/"       >=> text "Hello, World!" ]
+        route "/"       >=> razorView "text/html" "Home/Index" () ]
 
 let apiResources = [
     ApiResource("justin", "Test resource")
@@ -29,6 +31,9 @@ let clients = [
 
 type Startup() =
     member __.ConfigureServices (services : IServiceCollection) =
+        let env = services.BuildServiceProvider().GetService<IHostingEnvironment>()
+        let viewsFolderPath = Path.Combine(env.ContentRootPath, "Views")
+        services.AddRazorEngine(viewsFolderPath) |> ignore
         services.AddIdentityServer()
             .AddDeveloperSigningCredential()
             .AddInMemoryApiResources(apiResources)
@@ -37,10 +42,10 @@ type Startup() =
     member __.Configure (app : IApplicationBuilder)
                         (env : IHostingEnvironment)
                         (loggerFactory : ILoggerFactory) =
-        app.UseDeveloperExceptionPage()
-        app.UseIdentityServer()
-        app.UseStaticFiles()
-        app.UseGiraffe webApp
+        app.UseDeveloperExceptionPage() |> ignore
+        app.UseIdentityServer() |> ignore
+        app.UseStaticFiles() |> ignore
+        app.UseGiraffe webApp |> ignore
 
 [<EntryPoint>]
 let main argv =
